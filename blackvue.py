f512057d660f4548e6feebd7ecc45f69da505c61
bkmistry1 <bmistry1@stevens.edu>
2023-10-18 17:35:12 -0400

    adding gitignore and main download file

---
+++ blackvue.py
@@ -0,0 +1,76 @@
+import os
+import requests
+import shutil
+import asyncio
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ip = os.getenv("blackvueIP")
+print(ip, flush=True)
+
+def newName(fileString):
+    newName = fileString.replace("/Record/", "/")
+    return newName
+
+
+async def getFileList():
+    fileList = []
+
+    url = ip + "/blackvue_vod.cgi"
+
+    response = None
+    while(response == None or response.status_code != 200):
+        print("checking", flush=True)
+        try:
+            response = requests.get(url=url, timeout=5)
+        except Exception as e:
+            print(e, flush=True)
+        print("sleeping", flush=True)
+        await asyncio.sleep(10)
+    print(response.text, flush=True)
+    responseText = response.text
+
+    responseText = responseText.replace("v:1.00\r\n", "")
+    tmpText = ""
+    for i in range(len(responseText)-1):
+        tmpText += responseText[i]
+        if(responseText[i] == "\n"):
+            fileList.append(tmpText)
+            tmpText = ""
+
+    newList = []
+
+    for item in fileList:
+        newItem = item
+        newItem = newItem.replace(",s:1000000", "")            
+        newItem = newItem.replace("\r", "")            
+        newItem = newItem.replace("\n", "")
+        newItem = newItem.replace("n:", "")   
+        newList.append(newItem)
+
+    return newList
+
+async def main():
+    while(1):
+        fileList = await getFileList()
+
+        for item in fileList:
+            url = ip + str(item)
+            filePath = "/tmp" + newName(item)
+            print(filePath, flush=True)
+            try:
+                with requests.get(url=url, stream=True) as videoFile:
+                    videoFile.raise_for_status()
+                    
+                    with open(filePath, "wb") as newFile:
+                        for chunk in videoFile.iter_content(chunk_size=4096):
+                            newFile.write(chunk)
+
+                shutil.move(src=newFile.name, dst="/mnt/msd/Downloads"+newName(item))
+
+            except Exception as e:
+                print(e, flush=True)
+
+asyncio.run(main())
